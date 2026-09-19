fd9863d116de89520bc68464be0b91717f605965
yann bogdanovic <yannbogdanovic@MacBook-Air-de-yann.local>
2026-09-19 21:00:20 +0200

    fix

--- apps/web/src/help/opale/index.ts
+++ apps/web/src/help/opale/index.ts
@@ -1,3 +1,5 @@
+import { EBLC_HELP } from './eblc';
+import { YFDR_HELP } from './yfdr';
 import { YBALAC_HELP } from './ybalac';
 import { YBALAF_HELP } from './ybalaf';
 import { YGPIE1_HELP } from './ygpie1';
@@ -6,5 +8,7 @@ import type { OpaleSourceHelp, OpaleSourceId } from '../types';
 export const OPALE_HELP_REGISTRY: Record<OpaleSourceId, OpaleSourceHelp> = {
   YBALAC: YBALAC_HELP,
   YBALAF: YBALAF_HELP,
+  EBLC: EBLC_HELP,
+  YFDR: YFDR_HELP,
   YGPIE1: YGPIE1_HELP,
 };
